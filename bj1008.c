80d05a0ec65330fd343ad3685d5e872ef2d8a943
umia <agyejeong08@gmail.com>
2026-01-02 23:35:58 +0900

    feat: 문자열 입력 공부 (백준 10926번)

--- bj1008.c
+++ bj1008.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    double a,b;
+    double a,b;// 정수형은 int %d, 실수형은 double %lf
     scanf("%lf %lf",&a,&b);
     printf("%.9f\n",a/b);
     return 0;
-}git remote add origin https://github.com/umia01/TIL
+}
---
+++ bj10926.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main (){
+    char id[51];
+    scanf ("%s",id);//**문자열(String)**을 뜻하는 **%s**를 사용
+    printf("%s",id);
+    return 0;
+}
