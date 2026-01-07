9d7e6d964c580440c6f0531849f7fb0e6d336c8e
umia <agyejeong08@gmail.com>
2026-01-07 23:44:21 +0900

    Solve BOJ 1330: 두 수 비교하기

--- bj2588.c
+++ bj2588.c
@@ -1,27 +1,12 @@
-#include <iostream>
-
-using namespace std;
+#include <stdio.h>
 
 int main() {
-    int A, B;
-    
-    // Input the two numbers
-    cin >> A >> B;
-
-    // 1. A * ones digit of B
-    int step1 = A * (B % 10);
-    // 2. A * tens digit of B
-    int step2 = A * ((B / 10) % 10);
-    // 3. A * hundreds digit of B
-    int step3 = A * (B / 100);
-    // 4. Final result
-    int result = A * B;
-
-    // Output results
-    cout << step1 << endl;
-    cout << step2 << endl;
-    cout << step3 << endl;
-    cout << result << endl;
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a*(b%10));
+    printf("%d\n",a*((b/10)%10));
+    printf("%d\n",a*((b/100));
+    printf("%d\n",a*b);
 
     return 0;
 }
